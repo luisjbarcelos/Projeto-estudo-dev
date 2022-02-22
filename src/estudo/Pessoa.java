a16ece8ef99664e5c45062304049a6a976a3071e
Luis Henrique <luisjbarcelos@gmail.com>
2022-02-21 21:43:15 -0300

    ADICIONANDO ARQUIVOS

---
+++ src/estudo/Pessoa.java
@@ -0,0 +1,12 @@
+package estudo;
+
+public class Pessoa {
+
+	public	String nome;
+	private	int idade; 
+	protected String cpf;
+	
+	public void cadastrar () {
+		
+	};
+}
---
+++ src/estudo/principal.java
@@ -0,0 +1,10 @@
+package estudo;
+
+public class principal {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
